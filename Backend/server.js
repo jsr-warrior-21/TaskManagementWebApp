596e36a223f29d37db1bacc4aa3ac7db3d2cade8
Arvind <jsrarvind12@gmail.com>
2026-06-05 18:48:39 +0530

    Adding Frontend localhost link in backend

--- Backend/server.js
+++ Backend/server.js
@@ -10,12 +10,10 @@ const taskRoutes = require("./routes/tasks.routes.js");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    credentials: true,
-  }),
-);
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000'], 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
